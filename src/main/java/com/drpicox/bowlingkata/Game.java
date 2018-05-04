47e05d4780c92063c215273293510615880c1195
Roger Valero Polo <rvalero96@MacBook-Air-de-Roger-Valero.local>
2018-05-04 10:41:57 +0200

    primer test

--- src/main/java/com/drpicox/bowlingkata/Game.java
+++ src/main/java/com/drpicox/bowlingkata/Game.java
@@ -1,13 +1,4 @@
 package com.drpicox.bowlingkata;
 
 public class Game {
-    private int score = 0;
-
-    public void roll(int i) {
-        score +=i;
-    }
-
-    public int score() {
-        return score;
-    }
 }
--- src/test/java/com/drpicox/bowlingKata/BowlingGameTest.java
+++ src/test/java/com/drpicox/bowlingKata/BowlingGameTest.java
@@ -4,25 +4,7 @@ import com.drpicox.bowlingkata.Game;
 import junit.framework.TestCase;
 
 public class BowlingGameTest extends TestCase {
-    private Game g;
-
-    protected void setUp() throws Exception {
-        g = new Game();
-    }
-
     public void testGutterGame() throws Exception {
-        rollMany(20, 0);
-        assertEquals(0, g.score());
-    }
-
-    private void rollMany(int n, int pins) {
-        for (int i = 0; i < n; i++)
-            g.roll(pins);
-    }
-
-    public void testAllOnes() throws Exception {
-        for (int i = 0; i < 20; i++)
-            g.roll(1);
-        assertEquals(20, g.score());
+        Game g = new Game();
     }
 }
