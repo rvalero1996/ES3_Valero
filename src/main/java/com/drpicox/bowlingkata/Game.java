611bd103dc8c6f1e05726c61557b8cdb8c516a0d
Roger Valero Polo <rvalero96@MacBook-Air-de-Roger-Valero.local>
2018-05-04 11:21:35 +0200

    Diapositiva 36

--- src/main/java/com/drpicox/bowlingkata/Game.java
+++ src/main/java/com/drpicox/bowlingkata/Game.java
@@ -1,12 +1,10 @@
 package com.drpicox.bowlingkata;
 
 public class Game {
-    private int score = 0;
     private int rolls[] = new int[21];
     private int currentRoll = 0;
 
     public void roll(int pins) {
-        score += pins;
         rolls[currentRoll++] = pins;
     }
 
